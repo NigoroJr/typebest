c81bcc1348b750c3646a43d0fe74bc6a4f2e6e8d
Naoki Mizuno <nm2372@gmail.com>
2013-04-12 22:08:40 -0400

    TODO: Fix resetPanel method in MainTypePanel

--- src/MainTypePanel.java
+++ src/MainTypePanel.java
@@ -28,19 +28,30 @@ import java.io.FileNotFoundException;
  * @author Naoki Mizuno
  *
  */
-//TODO: Put the letters into a JLabel that represents the word so that the words will no be separated when coming to the end of the line.
+// TODO: UPDATE JAVADOC!!!!!
+// TODO: Modify tokenize() method so that it's easier to understand (try to think of a way to get rid of the tmp var)
+// TODO: Put the letters into a JLabel that represents the word so that the words will no be separated when coming to the end of the line.
 //|words being sep|
 //|arated among li|
 
-public class MainTypePanel extends JPanel implements KeyListener {
+public class MainTypePanel extends JPanel {
 
     public static final String FILENAME = "dic.txt";
+    
+    private Color toBeTyped = Color.BLUE;
+    private Color alreadyTyped = Color.RED;
+    // I didn't make this final so that the user can change it later
+    private Font defaultFont = new Font("MS Gothic", Font.BOLD, 30);
+    
     private int totalNumOfLetters = 0;
     private ArrayList<String> words = new ArrayList<String>();
     private ArrayList<JLabel> labels = new ArrayList<JLabel>();
     private static int cnt = 0;
     private static int miss = 0;
     private boolean finished = false;
+    
+    private long startTime = -1;
+    private long endTime = -1;
 
     public MainTypePanel() {
         super();
@@ -48,71 +59,29 @@ public class MainTypePanel extends JPanel implements KeyListener {
         setSize(800, 400);
         setLayout(new FlowLayout());
         setBackground(Color.GRAY);
+        
+        // Read from dictionary file
+        readDic();
 
-//        JPanel p = new JPanel();
-//        p.setLayout(new FlowLayout());
+        // Tokenize the words read from the file
+        tokenize();
 
-        // Add words from dictionary
-        try {
-            Scanner input = new Scanner(new File(FILENAME));
-            while (input.hasNext())
-                words.add(input.next());
-        }
-        catch (FileNotFoundException e) {
-            // TODO: When no dictionary file is found
-            e.printStackTrace();
-        }
-
-        // Find out the total number of letters in the dictionary file
-        // Add 1 for the white space after the word
-        for (int i = 0; i < words.size(); i++)
-        	totalNumOfLetters += words.get(i).length() + 1;
-
-        // Separate the words into chunks of letters
-        for (int w = 0; w < words.size(); w++) {
-            String word = words.get(w);
-            for (int i = 0; i < word.length(); i++) {
-                JLabel tmp = new JLabel(Character.toString(word.charAt(i)));
-                tmp.setFont(new Font("Arial", Font.BOLD, 40));
-                tmp.setForeground(Color.BLUE);
-                labels.add(tmp);
-//                p.add(tmp);
-                this.add(tmp);
-            }
-            JLabel tmp = new JLabel(" ");
-            tmp.setFont(new Font("Arial", Font.BOLD, 40));
-            labels.add(tmp);
-//            p.add(tmp);
-            this.add(tmp);
-        }
-
-        // To listen for key press
-        addKeyListener(this);
-
-//        getContentPane().add(p);
-//        this.add(p);
     }
 
-    @Override
-    public void keyPressed(KeyEvent arg0) {
-    }
-
-    @Override
-    public void keyReleased(KeyEvent arg0) {
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-    	// Don't go any further if it' done
+    public void processPressedKey(char pressed) {
+    	// Don't go any further if it's done
     	if (finished)
     		return;
 
-        char pressed = e.getKeyChar();
-
         String s;
         if ((s = labels.get(cnt).getText()) != null) {
-            if (pressed == s.charAt(0))
-                labels.get(cnt).setForeground(Color.RED);
+            // Start timer
+            if (startTime == -1)
+            	startTime = System.nanoTime();
+            
+            if (pressed == s.charAt(0)) {
+                labels.get(cnt).setForeground(alreadyTyped);
+            }
             else {
             	miss++;
                 return;
@@ -124,10 +93,83 @@ public class MainTypePanel extends JPanel implements KeyListener {
 
         // Finish if the user finishes typing all the words
         // (subtracts 1 because the last word is always a white space)
-        if (cnt == totalNumOfLetters - 1)
+        if (cnt == totalNumOfLetters - 1) {
+        	// Record the time it took
+        	endTime = System.nanoTime();
+        	
+        	long duration = endTime - startTime;
             JOptionPane.showMessageDialog(null,
-                    "Congratulations, you successfully typed all the words with " + miss + " miss types!",
+                    "Time: " + (double)duration / 1000000000 + "\nMiss: " + miss,
                     "Mission Accomplished", JOptionPane.INFORMATION_MESSAGE);
+        }
         repaint();
     }
+
+    /**
+     * Clears the words and prepares for a new round.
+     */
+	public void resetPanel() {
+		// Clear everything
+		this.removeAll();
+		
+		// Set everything to default value
+		finished = false;
+		cnt = 0;
+		miss = 0;
+		totalNumOfLetters = 0;
+		startTime = -1;
+		endTime = -1;
+		
+		words.clear();
+		labels.clear();
+//		System.out.println(String.format("w: %d, l: %d", words.size(), labels.size()));
+		readDic();
+		tokenize();
+		
+		repaint();
+	}
+	
+	/**
+	 * Reads in words from a dictionary file and store them into an ArrayList.
+	 * TODO: Add shuffle and make the words appear randomly
+	 */
+	public void readDic() {
+        // Add words from dictionary
+        try {
+            Scanner input = new Scanner(new File(FILENAME));
+            while (input.hasNext())
+                words.add(input.next());
+        }
+        catch (FileNotFoundException e) {
+        	JOptionPane.showMessageDialog(null, "No dictionary file was found",
+        			"No Dictionary file", JOptionPane.OK_OPTION);
+        }
+
+        // Find out the total number of letters in the dictionary file
+        // Add 1 for the white space after the word
+        for (int i = 0; i < words.size(); i++)
+        	totalNumOfLetters += words.get(i).length() + 1;
+
+	}
+	
+	/**
+	 * Tokenize the words into chunks of letters that will then be set to the color "toBeTyped".
+	 */
+	public void tokenize() {
+        // Separate the words into chunks of letters
+        for (int w = 0; w < words.size(); w++) {
+            String word = words.get(w);
+            for (int i = 0; i < word.length(); i++) {
+                JLabel tmp = new JLabel(Character.toString(word.charAt(i)));
+                tmp.setFont(defaultFont);
+                tmp.setForeground(toBeTyped);
+                labels.add(tmp);
+                this.add(tmp);
+            }
+            JLabel tmp = new JLabel(" ");
+            tmp.setFont(defaultFont);
+            labels.add(tmp);
+            this.add(tmp);
+        }
+	}
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,8 +1,5 @@
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
@@ -23,6 +20,8 @@ import javax.swing.SpringLayout;
 public class MainWindow extends JFrame {
 	public static final String LAST_USER = "lastUser.dat";
 	private User user;
+	private MainTypePanel mtp = new MainTypePanel();
+	private JButton restart = new JButton("Restart");
 	
 	/**
 	 * Creates a new window with a panel that you type in, and a menu bar
@@ -39,13 +38,26 @@ public class MainWindow extends JFrame {
 		menuBar();
 		
 		// Window to type in
-		MainTypePanel mtp = new MainTypePanel();
 		springLayout.putConstraint(SpringLayout.NORTH, mtp, 15, SpringLayout.NORTH, mainPanel);
 		springLayout.putConstraint(SpringLayout.SOUTH, mtp, -40, SpringLayout.SOUTH, mainPanel);
 		springLayout.putConstraint(SpringLayout.WEST, mtp, 5, SpringLayout.WEST, mainPanel);
 		springLayout.putConstraint(SpringLayout.EAST, mtp, -5, SpringLayout.EAST, mainPanel);
+		
+        addKeyListener(new TypingResponder());
+		
+        springLayout.putConstraint(SpringLayout.EAST, restart, -8, SpringLayout.EAST, mainPanel);
+        springLayout.putConstraint(SpringLayout.SOUTH, restart, -8, SpringLayout.SOUTH, mainPanel);
+        
+        ButtonResponder br = new ButtonResponder();
+        restart.addActionListener(br);
+        // When this is true, all the typing gets redirected to the button
+        restart.setFocusable(false);
+		
+        // Add things to the main panel
+		mainPanel.add(restart);
 		mainPanel.add(mtp);
 		
+        // Add to the frame
 		getContentPane().add(mainPanel);
 		
 		// The size of the main window
@@ -112,10 +124,37 @@ public class MainWindow extends JFrame {
 		
 	}
 	
+	/**
+	 * When a key is pressed, check if it's the right key
+	 */
+	public class TypingResponder implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {
+		}
+	
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+	
+		@Override
+		public void keyTyped(KeyEvent e) {
+			mtp.processPressedKey(e.getKeyChar());
+		}
+	}
+	
 	public static void main(String[] args) {
 		MainWindow mw = new MainWindow();
 		mw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mw.setLocationRelativeTo(null);
 		mw.setVisible(true);
+	}
+
+	public class ButtonResponder implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == restart)
+				mtp.resetPanel();
+		}
 	}
 }
