c8bbbefeb0a7d5c14c72568ad2ffc8290409c612
Naoki Mizuno <nm2372@gmail.com>
2013-04-18 19:34:21 -0400

    Modified User

--- src/User.java
+++ src/User.java
@@ -42,32 +42,9 @@ public class User {
 			s = new Settings(userName, read);
 		}
 		catch (FileNotFoundException e) {
-//			int choice = JOptionPane.showConfirmDialog(null,
-//					"No settings found.\nCreate a new setting file?", "No setting file found",
-//					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-//			
-//			// Can't proceed without creating a setting file
-//			if (choice == 1)
-//				System.exit(0);
-			
-			// Create a new Settings object with the default settings with only the userName
 			// TODO: Will actually write to a file when there is a change. Otherwise, it means the user is using the default settings.
 			s = new Settings(userName);
-			
-//			BufferedWriter bw;
-//			try {
-//				bw = new BufferedWriter(new FileWriter(userName + "_settings.dat"));
-//				
-//				bw.write(s.toString());
-//				
-//				bw.flush();
-//				bw.close();
-//			}
-//			catch (IOException newSettings) {
-//				JOptionPane.showMessageDialog(null, "Failed to create a new settings file.");
-//				System.exit(0);
-//			}
-		}	// End of catch when no setting file exists
+		}
 	}
 	
 	/**
