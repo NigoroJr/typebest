7174217d1f7c87bba2535524ca32214f95078407
Naoki Mizuno <nm2372@gmail.com>
2013-04-18 12:38:56 -0400

    Uses default settings when no setting file is found

--- src/User.java
+++ src/User.java
@@ -42,36 +42,38 @@ public class User {
 			s = new Settings(userName, read);
 		}
 		catch (FileNotFoundException e) {
-			int choice = JOptionPane.showConfirmDialog(null,
-					"No settings found.\nCreate a new setting file?", "No setting file found",
-					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+//			int choice = JOptionPane.showConfirmDialog(null,
+//					"No settings found.\nCreate a new setting file?", "No setting file found",
+//					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+//			
+//			// Can't proceed without creating a setting file
+//			if (choice == 1)
+//				System.exit(0);
 			
-			// Can't proceed without creating a setting file
-			if (choice == 1)
-				System.exit(0);
-			
-			// TODO: Write default settings or write to a file when there is a change in the setting?
 			// Create a new Settings object with the default settings with only the userName
+			// TODO: Will actually write to a file when there is a change. Otherwise, it means the user is using the default settings.
 			s = new Settings(userName);
-			BufferedWriter bw;
-			try {
-				bw = new BufferedWriter(new FileWriter(userName + "_settings.dat"));
-				
-				bw.write(s.toString());
-				
-				bw.flush();
-				bw.close();
-			}
-			catch (IOException newSettings) {
-				JOptionPane.showMessageDialog(null, "Failed to create a new settings file.");
-				System.exit(0);
-			}
+			
+//			BufferedWriter bw;
+//			try {
+//				bw = new BufferedWriter(new FileWriter(userName + "_settings.dat"));
+//				
+//				bw.write(s.toString());
+//				
+//				bw.flush();
+//				bw.close();
+//			}
+//			catch (IOException newSettings) {
+//				JOptionPane.showMessageDialog(null, "Failed to create a new settings file.");
+//				System.exit(0);
+//			}
 		}	// End of catch when no setting file exists
 	}
 	
 	/**
 	 * Reads the user's record from a file that has a filename {userName}_records.dat and
 	 * store it to the instance variable "r".
+	 * NOTE: make this method abstract and do everything in the Records class?
 	 */
 	public void readRecords() {
 		try {
@@ -86,10 +88,20 @@ public class User {
 	
 	/**
 	 * Returns the setting instance for this user.
-	 * TODO: make a clone method in Settings (as well as Records)
+	 * TODO: make a clone method in Settings
 	 */
 	public Settings getSettings() {
 		// return s.clone()
 		return s;
+	}
+	
+	/**
+	 * Returns the records instance for this user.
+	 * TODO: make a clone method in Records
+	 * NOTE: Is this really necessary? The Records class is used only to store records.
+	 */
+	public Records getRecords() {
+		// return s.clone()
+		return r;
 	}
 }
