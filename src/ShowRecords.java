5d79ac3a2aa7fe1beca2f02fd7c6f255752062f8
Naoki Mizuno <nm2372@gmail.com>
2013-05-20 11:51:06 +0900

    Working on GridBagLayout for showing the records

--- src/ShowRecords.java
+++ src/ShowRecords.java
@@ -1,5 +1,11 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 
 import javax.swing.JFrame;
@@ -16,20 +22,34 @@ public class ShowRecords {
 	
 	public ShowRecords(ArrayList<Result> records) {
 		JFrame frame = new JFrame("Results");
-		frame.setLayout(new GridLayout(0, 1));
-		
 		JPanel[] listOfResult = new JPanel[records.size()];
 		Collections.sort(records);
 		
-		// TODO: Add panels with the records to listOfResult (or the frame)
+		GridBagLayout gridBag = new GridBagLayout();
+		
+		GridBagConstraints oneResult = new GridBagConstraints();
 		for (int i = 0; i < records.size(); i++) {
 			Result r = records.get(i);
-			String resultText = String.format("%-3d %10f          %2d          %s          %s          %s", i + 1, r.getTime(), r.getMiss(), r.getUserName(), r.getKeyboardLayout(),r.getDate().getTime());
-			//System.out.println(records.get(i).toString());
+			String date = String.format("%2d/%2d/%4d %2d:%02d:%2d",
+					r.getDate().get(Calendar.MONTH), r.getDate().get(Calendar.DATE), r.getDate().get(Calendar.YEAR),
+					r.getDate().get(Calendar.HOUR_OF_DAY), r.getDate().get(Calendar.MINUTE), r.getDate().get(Calendar.SECOND));
+			String resultText = String.format("%-3d %10f    %2d %12s %8s    %s",
+					i + 1, r.getTime(), r.getMiss(), r.getUserName(), r.getKeyboardLayout(), date);
+			
 			JLabel label = new JLabel(resultText);
-			frame.add(label);
+			label.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));
+			label.setPreferredSize(new Dimension(label.getPreferredSize().width, label.getPreferredSize().height));
+			// Use random color to make it easier to see the boundaries
+			label.setBackground(new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256)));
+			label.setOpaque(true);
+			
+			oneResult.gridx = 0;
+			oneResult.gridy = i;
+			gridBag.setConstraints(label, oneResult);
+			
 		}
 		
+		frame.setLayout(gridBag);
 		frame.setLocationRelativeTo(null);
 		frame.setSize(800, 400);
 		// Simply moves the window so that it covers the MainWindow
