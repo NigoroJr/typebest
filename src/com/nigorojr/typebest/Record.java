1a0840c5b97ad0b6bc46074b982a74c7a8d3d9f3
Naoki Mizuno <nm2372@gmail.com>
2013-12-28 16:52:17 -0500

    Add Record class
    This class is a collection of data that consists one record. This is used when
    returning results of query of records in a form that is easy to read.

---
+++ src/com/nigorojr/typebest/Record.java
@@ -0,0 +1,13 @@
+package com.nigorojr.typebest;
+
+/**
+ * This class represents one result.
+ */
+
+public class Record {
+    public long user_id;
+    public String username;
+    public String keyboardLayout;
+    public long time;
+    public int miss;
+}
