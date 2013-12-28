0ffe5d427ada85b3d175e316b47f136facd7264f
Naoki Mizuno <nm2372@gmail.com>
2013-12-28 16:34:03 -0500

    Add inheritance from JPanel to Line class
    The Line class is directly added to the TypePanel, thus, it is better to
    extend JPanel.

--- src/com/nigorojr/typebest/Line.java
+++ src/com/nigorojr/typebest/Line.java
@@ -1,5 +1,9 @@
 package com.nigorojr.typebest;
 
-public class Line {
+import java.util.ArrayList;
 
+import javax.swing.JPanel;
+
+public class Line extends JPanel {
+    private ArrayList<Word> line = new ArrayList<Word>();
 }
