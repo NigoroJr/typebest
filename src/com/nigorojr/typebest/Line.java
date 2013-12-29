e4997b805b814c49da12e17cc2bd21b41c7fbe9e
Naoki Mizuno <nm2372@gmail.com>
2013-12-29 16:58:15 -0500

    Implement getWordsWidth method
    This method returns the width of the line in terms of the total length of the
    words and the spaces between the words. This information cannot be retrieved
    by using the inherited getWidth method, which simply returns the width of the
    line.

--- src/com/nigorojr/typebest/Line.java
+++ src/com/nigorojr/typebest/Line.java
@@ -2,8 +2,28 @@ package com.nigorojr.typebest;
 
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Line extends JPanel {
     private ArrayList<Word> line = new ArrayList<Word>();
+    private final JLabel space = new JLabel(" ");
+
+    /**
+     * Returns the total width of the words in the line by adding the length of
+     * the word and the width of the spaces between them.
+     * 
+     * @return The total width of the words in the line, including the spaces
+     *         between the words.
+     */
+    public int getWordsWidth() {
+        int width = 0;
+        for (int i = 0; i < line.size(); i++)
+            width += line.get(i).getWidth();
+
+        // Add total length of the spaces between words
+        width += space.getWidth() * (line.size() - 1);
+
+        return width;
+    }
 }
