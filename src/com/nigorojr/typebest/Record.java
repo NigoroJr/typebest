71d9cb8340c31340e5194d6634dafdacedf4afd1
Naoki Mizuno <nm2372@gmail.com>
2014-01-11 14:12:27 -0500

    Add constructor to Record class
    It is easier to create instances by using a constructor instead of
    substituting each variable one at a time.

--- src/com/nigorojr/typebest/Record.java
+++ src/com/nigorojr/typebest/Record.java
@@ -1,7 +1,8 @@
 package com.nigorojr.typebest;
 
 /**
- * This class represents one result.
+ * This class represents one result. It serves as a container when passing the
+ * results to a method/class.
  */
 
 public class Record {
@@ -10,4 +11,13 @@ public class Record {
     public String keyboardLayout;
     public long time;
     public int miss;
+
+    public Record(long user_id, String username, String keyboardLayout,
+            long time, int miss) {
+        this.user_id = user_id;
+        this.username = username;
+        this.keyboardLayout = keyboardLayout;
+        this.time = time;
+        this.miss = miss;
+    }
 }
